b03e334e583786f4c3bcdf898047d0d6744a82e2
crispwhistle189929 <59916058+crispwhistle189929@users.noreply.github.com>
2020-10-29 21:21:12 +0800

    Create fetch.js

---
+++ fetch.js
@@ -0,0 +1,51 @@
+
+import React from 'react';
+import {StyleSheet, SafeAreaView, Button, Text, FlatList} from 'react-native';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      title : "learn more", 
+      isClicked: false,
+    }
+    this.fetchData = this.fetchData.bind(this);
+  }
+  
+  //fetchData () {
+    
+  fetchData = async () => {
+    const res = await fetch("https://reactnative.dev/movies.json");
+    const json = await res.json();
+    
+    this.setState({isClicked: true});
+    this.setState({title: 'Data fetched!'});
+  }
+  
+  render() {
+    const {data} = this.state;
+
+    return(
+      <SafeAreaView style = {styles.container}>
+        <Button
+          disabled = {this.state.isClicked} 
+          onPress = {this.fetchData} 
+          title = {this.state.title} >
+        </Button>
+        
+      </SafeAreaView>
+    );
+  }
+
+}
+ 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'gray',
+    padding: 100,
+  },
+
+});
